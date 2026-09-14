046eb37523b248be289ca0db349f74add2a31a69
architraj99 <rajarchit99@gmail.com>
2026-09-14 16:39:34 +0530

    Made basic layout

---
+++ script.js
@@ -0,0 +1,61 @@
+const canvas = document.getElementById('main_screen');
+const context = canvas.getContext("2d");
+var pressedKeys = {};
+window.onkeyup = function(e) {
+    pressedKeys[e.keyCode] = false;
+};
+window.onkeydown = function(e) {
+    pressedKeys[e.keyCode] = true;
+};
+
+const titleScreen = document.getElementById("title-screen");
+const layers = [
+    document.getElementById("parallax-mountain-bg"),
+    document.getElementById("parallax-mountain-montain-far"),
+    document.getElementById("parallax-mountain-mountains"),
+    document.getElementById("parallax-mountain-trees"),
+    document.getElementById("parallax-mountain-foreground-trees")
+];
+const tiles = document.getElementById("tiles");
+const heroIdleAnimationLeft = document.getElementById("hero-idle-left");
+const heroIdleAnimationRight = document.getElementById("hero-idle-right");
+const heroRunAnimationLeft = document.getElementById("hero-run-left");
+const heroRunAnimationRight = document.getElementById("hero-run-right");
+const heroJumpAnimationLeft = document.getElementById("hero-jump-left");
+const heroJumpAnimationRight = document.getElementById("hero-jump-right");
+const heroAttackAnimationLeft = document.getElementById("hero-attack-left");
+const heroAttackAnimationRight = document.getElementById("hero-attack-right");
+const heroCrouchAnimationLeft = document.getElementById("hero-crouch-left");
+const heroCrouchAnimationRight = document.getElementById("hero-crouch-right");
+const heroHurtAnimationLeft = document.getElementById("hero-hurt-left");
+const heroHurtAnimationRight = document.getElementById("hero-hurt-right");
+
+let heroAnimations = [
+    [heroIdleAnimationLeft, 4],
+    [heroIdleAnimationRight, 4],
+    [heroRunAnimationLeft, 6],
+    [heroRunAnimationRight, 6],
+    [heroJumpAnimationLeft, 4],
+    [heroJumpAnimationRight, 4],
+    [heroAttackAnimationLeft, 5],
+    [heroAttackAnimationRight, 5],
+    [heroCrouchAnimationLeft, 1],
+    [heroCrouchAnimationRight, 1],
+    [heroHurtAnimationLeft, 1],
+    [heroHurtAnimationRight, 1]
+];
+
+let heroAnimationIndex = 1;
+let heroDirection = 1;
+let heroJumping = false;
+let heroPeakJumping = false;
+let jumpPressedLastFrame = false;
+let hoverjump = false;
+var scrollX = 0;
+var resetAnimationTime = Date.now();
+var previousHeroAnimationIndex = 1;
+let currentLevel = [];
+let points = 0;
+let gameStarted = false;
+let gameOver = false;
+let demoComplete = false;
