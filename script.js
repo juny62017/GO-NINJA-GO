d3fb047577d27322c0d47d341233ff888f918e50
architraj99 <rajarchit99@gmail.com>
2026-09-15 15:24:09 +0530

    Added tile collisions!

--- script.js
+++ script.js
@@ -591,3 +591,103 @@ var drawLevelTiles = function(level, hTiles, vTiles, scrollPosition) {
         }
     }
 };
+var checkSpriteTileCollision = function(sprite, tileX, tileY, tileIndex, collisions) {
+    if (
+        sprite.levelX + sprite.spriteWidth >= tileX &&
+        sprite.levelX <= tileX + TILE_SIZE &&
+        sprite.levelY + sprite.spriteHeight >= tileY &&
+        sprite.levelY <= tileY + TILE_SIZE
+    ) {
+        let intersect = {
+            x: Math.max(sprite.levelX, tileX),
+            y: Math.max(sprite.levelY, tileY)
+        };
+        intersect.width = Math.min(
+            sprite.levelX + sprite.spriteWidth,
+            tileX + TILE_SIZE
+        ) - intersect.x;
+        intersect.height = Math.min(
+            sprite.levelY + sprite.spriteHeight,
+            tileY + TILE_SIZE
+        ) - intersect.y;
+        if (
+            sprite.levelX + sprite.spriteWidth / 2 > tileX + TILE_SIZE / 2 &&
+            intersect.height > intersect.width
+        ) collisions.left = true;
+        if (
+            sprite.levelX + sprite.spriteWidth / 2 < tileX + TILE_SIZE / 2 &&
+            intersect.height > intersect.width
+        ) collisions.right = true;
+        if (
+            sprite.levelY + sprite.spriteHeight / 2 < tileY + TILE_SIZE / 2 &&
+            intersect.width > intersect.height &&
+            intersect.width > 4
+        ) {
+            collisions.top = true;
+            collisions.topY = tileY;
+        }
+        if (
+            sprite.levelY + sprite.spriteHeight / 2 > tileY + TILE_SIZE / 2 &&
+            intersect.width > intersect.height &&
+            intersect.width > 4
+        ) {
+            collisions.bottom = true;
+        }
+    }
+};
+
+var checkSpriteTileCollisions = function(sprite, level) {
+    let levelWidth = level[0].length;
+    let levelHeight = level.length;
+    let collisions = {
+        left: false,
+        right: false,
+        top: false,
+        bottom: false,
+        topY: null
+    };
+    for (let i = 0; i < levelWidth; i++) {
+        for (let j = 0; j < levelHeight; j++) {
+            if (level[j][i] != 0) {
+                checkSpriteTileCollision(
+                    sprite,
+                    i * TILE_SIZE,
+                    j * TILE_SIZE,
+                    level[j][i],
+                    collisions
+                );
+            }
+        }
+    }
+    return collisions;
+};
+
+var resetGame = function() {
+    hero = {
+        spriteWidth: 100,
+        spriteHeight: 59,
+        levelX: 140,
+        levelY: 480 - 32 - 59,
+        renderX: 100,
+        renderY: 480 - 32 - 59,
+        velocityY: 0
+    };
+    currentLevel = JSON.parse(JSON.stringify(levels[0]));
+    scrollX = 0;
+    points = 0;
+    hoverjump = false;
+    heroAnimationIndex = 1;
+    heroDirection = 1;
+    heroJumping = false;
+    heroPeakJumping = false;
+    jumpPressedLastFrame = false;
+    hoverjump = false;
+    scrollX = 0;
+    resetAnimationTime = Date.now();
+    previousHeroAnimationIndex = 1;
+    points = 0;
+    distanceFromFloor = 0;
+    nearestFloorHeight = -59;
+    newJumpPress = false;
+    attackCompleted = false;
+};
